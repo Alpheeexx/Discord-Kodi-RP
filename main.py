dd8c50f3358d97d0c83b52b0fb779b6d40f81534
AlphexGaming <40318243+AlphexGaming@users.noreply.github.com>
2019-02-11 13:27:00 +0000

    Removed asigning the same thing to a string like 3 times ;)

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from datetime import datetime, timedelta
 client_id = '535883147112611880'
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect()  # Start the handshake loop
-ip = str('http://localhost')
-port = str(8080)  # By default the port is 8080 if you have changed it specify here
-url = str(ip) + ':' + str(port) + '/jsonrpc'
+ip = 'http://localhost'
+port = 8080  # By default the port is 8080 if you have changed it specify here
+url = ip + ':' + str(port) + '/jsonrpc'
 infourl = "?request={%20%22jsonrpc%22:%20%222.0%22,%20%22method%22:%20%22Player.GetItem%22,%20%22params%22:%20{%20%22properties%22:%20[%20%22title%22,%20%22season%22,%20%22episode%22,%20%22duration%22,%20%22showtitle%22,%20%22tvshowid%22],%20%22playerid%22:%201%20},%20%22id%22:%20%22VideoGetItem%22%20}"
 lengthurl = "?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Player.GetProperties%22,%22params%22:{%22playerid%22:1,%22properties%22:[%22speed%22,%22time%22,%22totaltime%22]},%22id%22:%221%22}"
 result = None
